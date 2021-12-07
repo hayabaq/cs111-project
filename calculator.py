dd4d18dc89efd2cef8ca02c3fe55f98f5beeb34f
Mohammed Baqais <mohammedbaqais@Mohammeds-MacBook-Air.local>
2021-12-07 17:40:48 +0300

    update

--- calculator.py
+++ calculator.py
@@ -1,50 +1,82 @@
 import tkinter as tk
 app = tk.Tk()
 equation = ""
+result=""
 
 def clear():
-    global equation
+    global equation, result
     equation=""
+    result=""
     text.set(0)
 def sign():
-    #print(text.get())
-    if text.get() != 0:
-        tmp = int(text.get())* -1
-        text.set(tmp)
-        #print("hurray")
+    global equation, result
+    try:
+        tmp =text.get()
+        if tmp != 0:
+            result = int(tmp)* -1
+            equation=equation.replace(tmp,str(result))
+            text.set(result)
+    except:
+        text.set("Error")
+        result=""
+        equation=""
 def store(num):
-    global equation 
-    if num.isnumeric():
-        text.set(num)
-    equation= equation+str(num)
-    print(equation)
+    global equation, result
+    try:
+        if equation=="" and not num.isnumeric() and result!="":
+            equation=result+num
+        elif num.isnumeric():
+            text.set(num)
+            equation+=num
+        else:
+            equation+=num
+        if len(equation.split())==3:
+            calculate()
+    except:
+        text.set("Error")
+        equation=""
+        result="" 
 def calculate():
-    global equation 
-    result= str(eval(equation))
-    equation=result
-    text.set(result)
+    global equation , result
+    try:
+        if equation=="" and result=="":
+            text.set(str(0))  
+        else:    
+            result= str(eval(equation))
+            text.set(result)
+            equation=""
+    except:
+        text.set("Error")
+        equation=""
+        result=""
+
+
 text = tk.StringVar()
-screen = tk.Entry(app, textvariable =text).grid(row=0, column=0, columnspan=4 )
-clearbtn = tk.Button(app, text='AC', command=clear).grid(row=1, column=0)
-signbtn = tk.Button(app, text='+/-', command=sign).grid(row=1, column=1)
-remainderbtn = tk.Button(app, text='%', command=lambda:store('%')).grid(row=1, column=2)
-dividebtn = tk.Button(app, text='÷', command=lambda:store('/')).grid(row=1, column=3)
-btn7 = tk.Button(app, text='7', command=lambda:store('7')).grid(row=2, column=0)
-btn8 = tk.Button(app, text='8', command=lambda:store('8')).grid(row=2, column=1)
-btn9 = tk.Button(app, text='9', command=lambda:store('9')).grid(row=2, column=2)
-multiplybtn = tk.Button(app, text='x', command=lambda:store('*')).grid(row=2, column=3)
-btn4 = tk.Button(app, text='4', command=lambda:store('4')).grid(row=3, column=0)
-btn5 = tk.Button(app, text='5', command=lambda:store('5')).grid(row=3, column=1)
-btn6 = tk.Button(app, text='6', command=lambda:store('6')).grid(row=3, column=2)
-minusbtn = tk.Button(app, text='-', command=lambda:store('-')).grid(row=3, column=3)
-btn1 = tk.Button(app, text='1', command=lambda:store('1')).grid(row=4, column=0)
-btn2 = tk.Button(app, text='2', command=lambda:store('2')).grid(row=4, column=1)
-btn3 = tk.Button(app, text='3', command=lambda:store('3')).grid(row=4, column=2)
-plusbtn = tk.Button(app, text='+', command=lambda:store('+')).grid(row=4, column=3)
-btn0 = tk.Button(app, text='0', command=lambda:store('0')).grid(row=5, columnspan=2, column=1)
-dotbtn = tk.Button(app, text='.', command=lambda:store('.')).grid(row=5, column=2)
-equalbtn = tk.Button(app, text='=', command=calculate).grid(row=5, column=3)
+screen = tk.Entry(app, textvariable =text , state='readonly').grid(row=0, column=0, columnspan=4 ,sticky="nesw")
+clearbtn = tk.Button(app, text='AC',highlightbackground='grey', command=clear).grid(row=1, column=0,sticky="nesw")
+signbtn = tk.Button(app, text='+/-',highlightbackground='grey', command=sign).grid(row=1, column=1,sticky="nesw")
+remainderbtn = tk.Button(app, text='%',highlightbackground='grey', command=lambda:store(' % ')).grid(row=1, column=2,sticky="nesw")
+dividebtn = tk.Button(app, text='÷',highlightbackground='grey', command=lambda:store(' / ')).grid(row=1, column=3,sticky="nesw")
+btn7 = tk.Button(app, text='7',highlightbackground='grey', command=lambda:store('7')).grid(row=2, column=0,sticky='nesw')
+btn8 = tk.Button(app, text='8',highlightbackground='grey', command=lambda:store('8')).grid(row=2, column=1,sticky='nesw')
+btn9 = tk.Button(app, text='9',highlightbackground='grey', command=lambda:store('9')).grid(row=2, column=2,sticky='nesw')
+multiplybtn = tk.Button(app, text='x',highlightbackground='grey', command=lambda:store(' * ')).grid(row=2, column=3,sticky='nesw')
+btn4 = tk.Button(app, text='4',highlightbackground='grey', command=lambda:store('4')).grid(row=3, column=0,sticky='nesw')
+btn5 = tk.Button(app, text='5',highlightbackground='grey', command=lambda:store('5')).grid(row=3, column=1,sticky='nesw')
+btn6 = tk.Button(app, text='6',highlightbackground='grey', command=lambda:store('6')).grid(row=3, column=2,sticky='nesw')
+minusbtn = tk.Button(app, text='-',highlightbackground='grey', command=lambda:store(' - ')).grid(row=3, column=3,sticky='nesw')
+btn1 = tk.Button(app, text='1',highlightbackground='grey', command=lambda:store('1')).grid(row=4, column=0,sticky='nesw')
+btn2 = tk.Button(app, text='2',highlightbackground='grey', command=lambda:store('2')).grid(row=4, column=1,sticky='nesw')
+btn3 = tk.Button(app, text='3',highlightbackground='grey', command=lambda:store('3')).grid(row=4, column=2,sticky='nesw')
+plusbtn = tk.Button(app, text='+',highlightbackground='grey', command=lambda:store(' + ')).grid(row=4, column=3,sticky='nesw')
+btn0 = tk.Button(app, text='0',highlightbackground='grey', command=lambda:store('0')).grid(row=5, columnspan=2, column=0,sticky='nesw')
+dotbtn = tk.Button(app, text='.',highlightbackground='grey', command=lambda:store('.')).grid(row=5, column=2,sticky='nesw')
+equalbtn = tk.Button(app, text='=',highlightbackground='grey', command=calculate).grid(row=5, column=3,sticky='nesw')
 
 app.title('Calculator')
-#app.geometry('350x400')
+app.geometry('350x400')
+for i in range(6):
+    app.grid_rowconfigure(i,  weight =1)
+for i in range(4):
+    app.grid_columnconfigure(i,  weight =1)
 app.mainloop()
